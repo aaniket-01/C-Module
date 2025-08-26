90800eb8e6239196edddfc926a7c3a2cf2690d62
aaniket-01 <aniketswami.dev.19.@gmail.com>
2025-08-26 15:44:40 +0530

    finding power and prime number question

---
+++ Day-1-Aug-22/power.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+// int main()
+// {
+//     int n, pow, result = 1; 
+
+//     cout<<"Enter the number: "<<endl;
+//     cin>>n>>pow;
+
+//     for(int i=1; i<=pow; i++)
+//         result = result * n;
+
+//     cout<<"Factorial of a number: "<<result<<endl;
+//     return 0;
+// }
+
+int main()
+{
+    int m = 2, n = 3;
+
+    int result = pow(2,3);
+
+    cout<<"Result is: "<<result<<endl;
+
+    return 0;
+}
